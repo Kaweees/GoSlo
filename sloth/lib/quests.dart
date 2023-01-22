7fab714dc359f99c59a2b2922c5a0285d82b87e8
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-01-22 08:28:00 -0800

    Update quests.dart

--- sloth/lib/quests.dart
+++ sloth/lib/quests.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:sloth/quest_stops.dart';
 
 class Quests extends StatefulWidget {
-  const Quests({super.key});
+  const Quests({Key? key}) : super(key: key);
 
   @override
   State<Quests> createState() => _QuestsState();
@@ -12,7 +14,148 @@ class _QuestsState extends State<Quests> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(),
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Align(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.38,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(top: 15),
+                        child: Text(
+                          "Start Quest",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(
+                          top: 20,
+                          left: 20,
+                          right: 20,
+                        ),
+                        padding: const EdgeInsets.only(
+                          top: 10,
+                          bottom: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                          boxShadow: const [
+                            BoxShadow(
+                              blurRadius: 7,
+                              color: Color(0x53000000),
+                              offset: Offset(0, 2),
+                            )
+                          ],
+                          border: Border.all(
+                            width: 2,
+                            color: const Color.fromRGBO(21, 71, 52, 1),
+                          ),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 75,
+                              width: 75,
+                              margin: const EdgeInsets.only(left: 15),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(
+                                    fit: BoxFit.cover,
+                                    "https://www.slocity.org/home/showpublishedimage/18503/637992694344400000"),
+                              ),
+                            ),
+                            Container(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 15),
+                                    child: SizedBox(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.6,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const Text(
+                                            "San Luis Obispo Sightseeing Quest",
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 18,
+                                            ),
+                                          ),
+                                          Container(
+                                            margin:
+                                                const EdgeInsets.only(top: 5),
+                                            child: Row(
+                                              children: [
+                                                SizedBox(
+                                                  width: 110,
+                                                  child: Container(
+                                                    child: const ClipRRect(
+                                                      borderRadius:
+                                                          BorderRadius.all(
+                                                        Radius.circular(10),
+                                                      ),
+                                                      child:
+                                                          LinearProgressIndicator(
+                                                        minHeight: 5,
+                                                        value: 0.7,
+                                                        valueColor:
+                                                            AlwaysStoppedAnimation<
+                                                                Color>(
+                                                          Color(0xff00ff00),
+                                                        ),
+                                                        backgroundColor:
+                                                            Color(0xffD6D6D6),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                                const Padding(
+                                                  padding: EdgeInsets.only(
+                                                    left: 15,
+                                                  ), //apply padding to all four sides
+                                                  child: Text(
+                                                    "20%",
+                                                    style: TextStyle(
+                                                      color: Colors.black87,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
